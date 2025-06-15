bb3fc22e126091ad18dc5120b6bbc3cd689acc3f
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-15 22:43:07 +0530

    displayblog view is complete

--- Blogs/resources/views/DisplayBlogs.blade.php
+++ Blogs/resources/views/DisplayBlogs.blade.php
@@ -9,7 +9,42 @@
 </head>
 
 <body>
-    <h1 class="bg-red-500">hello</h1>
+    <div class="h-screen w-screen p-4 flex justify-center items-center">
+        <div class="h-full w-[50%] p-2  shadow-2xl shadow-black/50 overflow-auto">
+            <img src="{{ url('exampleimgs/PNiEmf25aief43shQfe1cgQEqY00TeNpPpZKqv7c.jpg') }}" alt="blog image" class="h-[50%] w-full object-cover">
+            <div class="p-2 font-bold">Bhavesh : 15-6-2025 </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione nisi deleniti sequi sed? Molestiae dicta corporis autem ipsum est omnis repellat et consequuntur minus vitae libero officia, alias aliquid sit.
+                Id, voluptatem. Aperiam aliquam nihil, labore quasi laudantium et necessitatibus est excepturi fuga repellendus, ab eveniet ducimus cupiditate repellat commodi accusantium quos itaque eum ipsa consequatur reiciendis voluptate dolores. Corrupti!
+                Optio, repudiandae. At repudiandae harum sed distinctio cupiditate sequi eaque sit sunt eius maxime sapiente, totam illo animi, magnam id libero aliquam? Omnis vel repudiandae rerum veniam molestias itaque quae?
+                Quis consectetur veritatis totam nemo, nostrum maxime cupiditate dolorum. At possimus qui explicabo consequuntur tenetur. Earum nihil officia sit veritatis delectus voluptas, nisi accusantium nobis placeat recusandae ipsa fuga provident!
+                Dolorem eius quisquam, ad eaque voluptatibus expedita esse quibusdam inventore facilis qui in minima veritatis accusantium odio at debitis unde cumque odit excepturi adipisci quia. Sed eligendi quis quia maiores.
+                In id blanditiis nemo natus consequuntur aliquam eligendi error, minima voluptates amet laboriosam perferendis nesciunt reprehenderit eaque! Nisi modi fugiat rem quibusdam placeat. Eligendi pariatur vero officia, ad dolores exercitationem!
+                Natus, officiis. Nihil quo sapiente ducimus eaque explicabo sit obcaecati, sunt iusto veritatis itaque dolores pariatur voluptate dolore eum reiciendis deserunt aliquid? Quod molestiae ab excepturi labore optio voluptate ipsum?
+                Alias, soluta possimus reprehenderit expedita atque illum, iusto delectus nihil enim doloribus mollitia fugit porro quam eum a quaerat necessitatibus. Illum odit deserunt minus minima tenetur, dolor vitae? Qui, natus.
+                Libero suscipit facilis culpa architecto aperiam quaerat ex optio nisi. Iste eligendi iure quaerat officiis, dolorem, nostrum vel, quisquam delectus ex incidunt non fugiat doloribus nam culpa! Fugit, praesentium pariatur!
+                Ducimus aliquid eum ipsa quidem molestiae magnam, ipsam obcaecati dolor fugiat libero pariatur veniam voluptate similique ullam soluta reiciendis nemo animi possimus iste quis officia, illo dignissimos suscipit laboriosam! Quaerat!
+            </p>
+            <br><br>
+            <div class="flex flex-wrap gap-4">
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+                <span class="bg-gray-400 shadow-sm shadow-black/50">#bhavesh</span>
+            </div>
+            <br><br><br><br>
+            <div class="flex justify-end">
+                <a href="" class="bg-purple-500 text-white text-right p-4 ">About This Blog</a>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
