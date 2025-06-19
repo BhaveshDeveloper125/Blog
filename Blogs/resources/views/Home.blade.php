e7ce4c9fab8f500a93b0a9713dbcdd9203569054
BhaveshDeveloper125 <bhavesh2103m@gmail.com>
2025-06-19 12:37:09 +0530

    authentication is done

--- Blogs/resources/views/Home.blade.php
+++ Blogs/resources/views/Home.blade.php
@@ -114,23 +114,22 @@
 </body>
 </html>
 
-    <!-- <form action="/postBlog" method="post" enctype="multipart/form-data" class="max-w-sm mx-auto">
-        @csrf
-        <div class="mb-5">
-            <label class="block mb-2 text-sm font-medium text-gray-900" for=" file_input">Select Image</label>
-            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none" id="file_input" type="file">
-        </div>
-        <div class="mb-5">
-            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 ">Blog Title</label>
-            <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="Blog Title" required />
-        </div>
-        <div class="mb-5">
-            <label for="author" class="block mb-2 text-sm font-medium text-gray-900 ">Blog Author Name (Your Name)</label>
-            <input type="text" name="author" id="author" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
-        </div>
-        <div class="mb-5">
-            <textarea name="content" id="summernote" placeholder="Enter Blog content"></textarea>
-        </div>
-
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
-    </form> -->
+<!-- <form action="/postBlog" method="post" enctype="multipart/form-data" class="max-w-sm mx-auto">
+    @csrf
+    <div class="mb-5">
+        <label class="block mb-2 text-sm font-medium text-gray-900" for=" file_input">Select Image</label>
+        <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50  focus:outline-none" id="file_input" type="file">
+    </div>
+    <div class="mb-5">
+        <label for="title" class="block mb-2 text-sm font-medium text-gray-900 ">Blog Title</label>
+        <input type="text" name="title" id="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  " placeholder="Blog Title" required />
+    </div>
+    <div class="mb-5">
+        <label for="author" class="block mb-2 text-sm font-medium text-gray-900 ">Blog Author Name (Your Name)</label>
+        <input type="text" name="author" id="author" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " required />
+    </div>
+    <div class="mb-5">
+        <textarea name="content" id="summernote" placeholder="Enter Blog content"></textarea>
+    </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
+</form> -->
--- Blogs/routes/web.php
+++ Blogs/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\PublishBlogs;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,23 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::post('/postBlog', [PublishBlogs::class, 'Blogs'])->name('submit.blog');
-Route::post('/upload-image', [PublishBlogs::class, 'uploadImage']);
-
-Route::post('/filter', [PublishBlogs::class, 'Filteration']);
-
-Route::get('/allblogs', [PublishBlogs::class, 'GetBlogs']);
-Route::get('/blogs/{id}', [PublishBlogs::class, 'SpecificBlog']);
-Route::get('/aboutblog/{id}', [PublishBlogs::class, 'AboutBlog']);
-Route::get('/filter', action: function () {
-    return redirect('/');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Route::view('/blogs', 'DisplayBlogs');
-// Route::view('/allblogs', 'AllBlogList');
-// Route::view('/aboutblog', 'AboutBlog');
-Route::view('/home', 'Home');
+require __DIR__.'/auth.php';
